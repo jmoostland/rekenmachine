bc20bf7470cc43e150e775155f92067f1f1e7e88
jmoostland <jeanineoostland@gmail.com>
2017-10-29 10:10:08 +0100

    htmltwee

--- index.php
+++ index.php
@@ -7,17 +7,35 @@ switch gebruiken-->
         <title>Page Title</title>
     </head>
     <body>
-
-        <h1>Rekenmachine</h1>
-        <p>Vul hier het eerst getal in:</p>
-
-    </body>
+        <div style="color:#330099">
+            <h1>Rekenmachine</h1>
+        </div>
+        <div style="color:#990099">
+            <p>Vul hier de getallen in:</p>
+        </div>
+        <form index="index.php">
+            Eerste getal: <input type="text" name="getal1">
+            <br>
+            <br>
+            <select name="operator">
+                <option name="optellen">+
+                <option name="aftrekken">-
+            <option name="vermenigvuldigen">*
+        <option name="delen">/
+</select>
+<br>
+<br>
+Tweede getal: <input type="text" name="getal2">
+<br>
+<input type="submit" value="uitkomst">
+</form>
+</body>
 </html>
 
 <?php
-$getal1 = $_GET ['getal1'];
-$getal2 = $_GET ['getal2'];
-$operator = $_GET['operator'];
+$getal1 = $_POST ['getal1'];
+$getal2 = $_POST ['getal2'];
+$operator = $_POST['operator'];
 
 function optellen($getal1, $getal2) {
     echo "$getal1 + $getal2 = " . ($getal1 + $getal2);
@@ -48,7 +66,7 @@ function even_getal($getal1, $getal2) {
     return ($getal1 + $getal2 % 2) ? false : true;
 }
 
-//Dezelfde volgorde wordt aangehouden bij de if els (dus eerst false en oneven, etc)
+//Dezelfde volgorde wordt aangehouden bij de if else (dus eerst false en oneven, etc)
 
 function exponent($getal1, $getal2) {
     echo"$getal1 ** $getal2 = " . ($getal1 ** $getal2);
@@ -57,7 +75,8 @@ function exponent($getal1, $getal2) {
 
 switch ($operator) {
     default:
-        echo"<b>Je mag een keuze maken uit: optellen, aftrekken, vermenigvuldigen, delen, modulo en exponent.</br>";
+        echo"<b><dl><dt>" .
+        "Je mag een keuze maken uit:<dt><dd> optellen</dd><dd> aftrekken</dd> <dd>vermenigvuldigen</dd><dd> delen</dd><dd>modulo</dd><dd>exponent.</dd></br>";
         break;
     case "optellen":
         echo optellen($getal1, $getal2);
@@ -116,7 +135,8 @@ switch ($operator) {
         <br>
         <a href="https://github.com/jmoostland">Voor meer informatie</a>
         <br>
+        <!--        Waarom wordt de link paars?-->
         <br>
-<button type="button" onclick="alert('Om <button> te testen')">Click Me!</button>
+        <button type="button" onclick="alert('Om <button> te testen')">Klik hier!</button>
     </body>
 </html>
